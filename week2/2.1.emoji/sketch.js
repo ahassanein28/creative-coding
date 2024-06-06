9bd7d41f46eea00ff64ac95c415e41b5f2ec4d12
ahassanein28 <ahassane@mail.umw.edu>
2024-06-06 23:40:50 +0100

    sad crying face!

--- week2/2.1.emoji/sketch.js
+++ week2/2.1.emoji/sketch.js
@@ -4,4 +4,29 @@ function setup() {
 
 function draw() {
   background(220);
+
+
+  stroke("gold"); // set the stroke (line) color
+  strokeWeight(6); // set the stroke thickness
+  fill("yellow"); // set the fill color 
+
+  // draw an ellipse with 250 pixel diameter
+  // with its center at x position 200
+  // and at y position 200
+  ellipse(200,200,250,250)
+
+  // draw the eyes 
+  fill(0);
+  ellipse(160, 180, 40, 40);
+  ellipse(240, 180, 40, 40);
+
+  // draw the mouth
+  noFill();
+  stroke(0);
+  arc(200, 280, 120, 80, PI + 0.1, TWO_PI - 0.1); // arc is mouth
+
+  // draw the tears 
+  fill("blue")
+  ellipse(160, 210, 20, 80);
+  ellipse(240, 210, 20, 80);
 }
