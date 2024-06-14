bc7f268d2da0a3433f321ed5ac90bde6f398993d
ahassanein28 <ahassane@mail.umw.edu>
2024-06-14 12:40:13 -0400

    Update sketch.js
    Edited to recreate art work for assignment

--- week2/2.3.emulation/sketch.js
+++ week2/2.3.emulation/sketch.js
@@ -1,7 +1,58 @@
+// My attemt of this art work 
+// Title: Meules No. 7 (From the Homage to Monet's 
+// "Meules" Series 
+// Artist: Vera Molnar 
+// Programmed in 1977 
+// https://spalterdigital.com/artworks/meules-no-7/
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1225, 1225);
+  noLoop();
 }
 
 function draw() {
-  background(255);
+
+  // matching the background color 
+  // based on eyedropper tool 
+  background(232,238,238);  
+
+  // labeling the specific colors 
+  // needed using eyedropper tool 
+  // source: https://editor.p5js.org/allison.parrish/sketches/9mz4_ot5b
+  const colors = [
+    color(198, 81, 64),
+    color(42, 103, 98),
+    color(189,129,137)
+    ];
+
+  // push the grid out of the corner 
+  translate(50,50);
+
+
+
+  for (let x = 0; x < 24; x += 1) {
+    for(let y = 0; y < 24; y += 1) {
+      push();
+
+      // move the lines closer to eachother
+      translate(x * 50, y * 50);
+
+      // get rid of the borders 
+      noStroke();
+
+      // fill colors randomly baseed 
+      // on options of three colors 
+      fill(random(colors));
+
+      // make the lines rotate randomly
+      rotate(random(360));
+
+      // draw the bubble(???) lines
+      ellipse (2,2, 160, 10);
+
+      pop();
+    }
+  }
+
 }
