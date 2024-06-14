8be8635dbeb7aca00620f46852ebafedfdd36045
ahassanein28 <ahassane@mail.umw.edu>
2024-06-14 11:15:01 -0400

    Update sketch.js
    Followed assignment directions

--- week2/2.2.grid/sketch.js
+++ week2/2.2.grid/sketch.js
@@ -1,7 +1,36 @@
 function setup() {
   createCanvas(1200, 1200);
+  noLoop();
 }
 
 function draw() {
+  // setting the background to white
   background(220);
+
+  // push the grid out of the corner more centered 
+  translate(215,215);
+
+  for (let x = 0; x < 8; x += 1) {
+    for(let y = 0; y < 8; y += 1) {
+      push();
+
+      // move the drawing plan to 
+      // add a new circle
+      translate(x * 110, y * 110);
+
+      // line thickness 
+      strokeWeight(5);
+
+      // line color  
+      stroke(random(0,255),128,228);
+
+      // fill color 
+      fill(238, random(0,255), 238);
+
+      // draw the circle
+      ellipse (0,0, random(50,100),100);
+
+      pop();
+    }
+  }
 }
